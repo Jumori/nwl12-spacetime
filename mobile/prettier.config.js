07f199b53ce5af9af50e04c7fece658833ec0117
Juliana Morikoshi <julianamorikoshi@gmail.com>
2023-05-16 01:05:17 -0300

    chore: init mobile project

---
+++ mobile/prettier.config.js
@@ -0,0 +1,10 @@
+{
+}
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'auto',
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
